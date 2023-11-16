ff13595481e789f38258318fe2cfbd79db289c03
chris85gillis <chris85gillis@gmail.com>
2023-11-16 13:21:26 -0800

    removed white space

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -23,7 +23,7 @@ def get_employee_todo_progress(employee_id):
 
     print(f"Employee {employee_name} is done with tasks(", end="")
     print(f"{num_completed_tasks}/{total_tasks}):")
-    
+
     for task in completed_tasks:
         print(f"\t{task['title']}")
 
