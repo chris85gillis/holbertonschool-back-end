55ef149aa6517cb8be21f86bb18874b1651307e5
chris85gillis <chris85gillis@gmail.com>
2023-11-19 15:13:57 -0800

    cleaned up files to be easier to read

--- api/3-dictionary_of_list_of_dictionaries.py
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -1,19 +1,35 @@
 #!/usr/bin/python3
-"""import"""
-import json
-import requests
+"""Retrieve data in JSON format"""
 
-if __name__ == "__main__":
-    URL = "https://jsonplaceholder.typicode.com"
+from sys import argv
+from requests import get
+from json import dump
 
-    users = requests.get(f"{URL}/users").json()
-    dic_user = {}
+url_base = 'https://jsonplaceholder.typicode.com/users/'
+
+
+def dict_of_the_dict():
+    """Retrieves and organizes todo tasks for each user"""
+    users = get(url_base).json()
+    retrieve_json = dict()
+
     for user in users:
-        tasks = requests.get(f"{URL}/users/{user['id']}/todos").json()
-        dic_user[user["id"]] = []
-        for task in tasks:
-            dic_task = {"task": task["title"], "completed": task["completed"],
-                        "username": user["username"]}
-            dic_user[user["id"]].append(dic_task)
-    with open("todo_all_employees.json", "w") as file:
-        json.dump(dic_user, file)
+        usr_id = user['id']
+        item_data = []
+        task_users = get(url_base + str(usr_id) + '/todos/').json()
+
+        for todo in task_users:
+            item_dict = {
+                'task': todo['title'],
+                'completed': todo['completed'],
+                'username': user['username']
+            }
+            item_data.append(item_dict)
+        retrieve_json[usr_id] = item_data
+
+    with open('todo_all_employees.json', 'w', encoding='utf-8') as file_json:
+        dump(retrieve_json, file_json)
+
+
+if __name__ == '__main__':
+    dict_of_the_dict()
