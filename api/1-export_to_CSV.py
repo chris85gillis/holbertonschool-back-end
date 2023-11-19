31f7fb2dcbdd7540119a5f8d2bcc7ab7823be2f0
chris85gillis <chris85gillis@gmail.com>
2023-11-19 14:46:44 -0800

    adding changes to file 1 for better checks

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -4,52 +4,25 @@ import csv
 import requests
 import sys
 
-
-def get_employee_todo_progress(USER_ID):
-    """constructing URLs for user and todos data"""
-    base_url = 'https://jsonplaceholder.typicode.com/'
-    user_url = f'{base_url}users/{USER_ID}'
-    todos_url = f'{base_url}todos?userId={USER_ID}'
-
-    user_response = requests.get(user_url)
-    user_data = user_response.json()
-    employee_name = user_data.get('name')
-
-    todos_response = requests.get(todos_url)
-    todos_data = todos_response.json()
-
-    """Extract relevant data"""
-    task_records = []
-    for task in todos_data:
-        task_record = {
-            "USER_ID": USER_ID,
-            "USERNAME": employee_name,
-            "TASK_COMPLETED_STATUS": task['completed'],
-            "TASK_TITLE": task['title']
-        }
-        task_records.append(task_record)
-
-    return task_records
-
-
-def write_to_csv(task_records, csv_file_name):
-    with open(csv_file_name, 'w', newline='') as csvfile:
-        fieldnames = ["USER_ID", "USERNAME",
-                      "TASK_COMPLETED_STATUS",
-                      "TASK_TITLE"]
-        writer = csv.DictWriter(csvfile, fieldnames=fieldnames,
-                                quoting=csv.QUOTE_ALL)
-        writer.writerows(task_records)
-
-
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python3 script_name.py <USER_ID>")
+    if len(sys.argv) < 2:
+        print(f"missing employee id as argument")
         sys.exit(1)
 
-    USER_ID = int(sys.argv[1])
-    task_records = get_employee_todo_progress(USER_ID)
+    URL = "https://jsonplaceholder.typicode.com"
+    EMPLOYEE_ID = sys.argv[1]
 
-    """Specify a CSV file name"""
-    csv_file_name = f"{USER_ID}.csv"
-    write_to_csv(task_records, csv_file_name)
+    EMPLOYEE_TODOS = requests.get(f"{URL}/users/{EMPLOYEE_ID}/todos",
+                                  params={"_expand": "user"})
+    data = EMPLOYEE_TODOS.json()
+
+    EMPLOYEE_NAME = data[0]["user"]["username"]
+    fileName = f"{EMPLOYEE_ID}.csv"
+
+    with open(fileName, "w", newline="") as file:
+        writer = csv.writer(file, quoting=csv.QUOTE_NONNUMERIC)
+        for task in data:
+            writer.writerow(
+                [EMPLOYEE_ID, EMPLOYEE_NAME, str(task["completed"]),
+                 task["title"]]
+            )
